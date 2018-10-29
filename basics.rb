033b811d8c4395a5f54522ab0425c94f98a0b06f
analogy47 <actweed47@gmail.com>
2018-10-29 14:46:56 -0400

    create basic commands file

---
+++ basics.rb
@@ -0,0 +1,13 @@
+# example of number, string, and boolean data types
+
+data_num =  47   
+
+data_boolean =  true   
+
+data_string =  "fantastic!"   
+
+# puts adds a blank line (vs. print --just outputs to screen)
+puts data_num
+puts data_boolean
+puts data_string
+
