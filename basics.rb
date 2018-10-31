e196652b0ec3775d7fcfe06e26385308957d623f
analogy47 <actweed47@gmail.com>
2018-10-31 17:25:10 -0400

    add basic commands

--- basics.rb
+++ basics.rb
@@ -11,3 +11,17 @@ puts data_num
 puts data_boolean
 puts data_string
 
+
+# methods
+
+# .length
+day_string = 'Tuesday'
+puts day_string.length # <-- 7
+
+# .reverse
+rev_string = 'desrever'
+puts rev_string.reverse # <-- 'reversed'
+
+# .upcase and .downcase
+puts day_string.upcase
+puts 'LAND MASSES'.downcase
