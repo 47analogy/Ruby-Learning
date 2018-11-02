a200132fbf6b895c46e140800d33e6142baeb1e1
analogy47 <actweed47@gmail.com>
2018-11-02 13:10:29 -0400

    add basics control flow

--- basics.rb
+++ basics.rb
@@ -1,3 +1,6 @@
+
+#=================================================================
+
 # example of number, string, and boolean data types
 
 data_num =  47   
@@ -11,17 +14,72 @@ puts data_num
 puts data_boolean
 puts data_string
 
+=begin
+gets user input via keyboard
+.chomp removes newline (\n)
+gets.chomp
+=end
+
+puts "what is you favorite number"
+fav_num = gets.chomp
+# puts fav_num
+
+
 
 # methods
 
 # .length
-day_string = 'Tuesday'
+day_string = "Tuesday"
 puts day_string.length # <-- 7
 
 # .reverse
-rev_string = 'desrever'
-puts rev_string.reverse # <-- 'reversed'
+rev_string = "desrever"
+puts rev_string.reverse # <-- "reversed"
 
 # .upcase and .downcase
-puts day_string.upcase
-puts 'LAND MASSES'.downcase
+puts day_string.upcase # <-- "Tuesday"
+puts "LAND MASSES".downcase # <-- "land masses"
+
+# .capitalize
+puts "important".capitalize # <-- "Important"
+
+# methods that end with ! modifies a variable in-place
+car = "toyota"
+puts car # <-- "toyota"
+
+car.capitalize
+puts car # <-- "toyota"
+
+car.capitalize!
+puts car # <-- "Toyota" --variable is changed
+
+
+
+# String interpolation
+color = "purple"
+puts "The shirt is #{color}"
+
+#=================================================================
+
+# Examples of Control Flow
+
+print "Integer please: "
+user_num = Integer(gets.chomp) 
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end # <-- "end of control block"
+
+
+# unless statement is used to check if something is false
+Friday = false
+
+unless Friday
+  puts "Today is not Friday"
+else
+  puts "Today is Friday"
+end
